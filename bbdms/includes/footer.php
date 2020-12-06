5f37422374ad2afec1610529901d0655751494a6
brijesh-menon <75576717+brijesh-menon@users.noreply.github.com>
2020-12-06 21:09:02 +0530

    Add files via upload

---
+++ bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white"> BloodBank & Donor Management System 2020 - Group No. 3</p>
+        </div>
+        <!-- /.container -->
+    </footer>
